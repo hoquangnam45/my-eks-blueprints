b01c4c15f267d166cec2845c4bcbc2df7b289a78
EC2 Default User <ec2-user@ip-172-31-41-241.ap-southeast-1.compute.internal>
2022-06-09 06:08:32 +0000

    Setting up EKS Blueprints deployment pipeline

---
+++ lib/pipeline.ts
@@ -0,0 +1,35 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+
+export default class PipelineConstruct extends Construct {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps){
+    super(scope,id)
+
+    const account = props?.env?.account!;
+    const region = props?.env?.region!;
+
+    const blueprint = blueprints.EksBlueprint.builder()
+    .account(account)
+    .region(region)
+    .addOns()
+    .teams();
+  
+    blueprints.CodePipelineStack.builder()
+      .name("eks-blueprints-workshop-pipeline")
+      .owner("hoquangnam45")
+      .repository({
+          repoUrl: 'my-eks-blueprints',
+          credentialsSecretName: 'eks-workshop-token',
+          targetRevision: 'main'
+      })
+      // WE ADD THE STAGES IN WAVE FROM THE PREVIOUS CODE
+      .wave({
+        id: "envs",
+        stages: [
+          { id: "dev", stackBuilder: blueprint.clone('ap-southeast-1')}
+        ]
+      })
+      .build(scope, id+'-stack', props as any);
+  }
+}
